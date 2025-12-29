d842ec2cf97ba46f30d80ca7f7f1ee71fc72dd8e
thisvolwebnik(Nikita) <thisvolwebnik@yandex.ru>
2025-12-29 21:54:28 +0300

    feat: implement sticky header with scroll animation timeline

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,11 @@
+import "./Header.scss";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      <div className="header__inner container"></div>
+    </header>
+  );
 };
 
 export default Header;
