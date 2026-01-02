f13ff97a6f9fb5b23566e75b9ffa993ec630f03f
thisvolwebnik(Nikita) <thisvolwebnik@yandex.ru>
2026-01-02 14:02:34 +0300

    feat: add Categories section with CategoryCard and Image components

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,9 +1,57 @@
 import "./Categories.scss";
 import classNames from "classnames";
 import Section from "@/layouts/Section";
+import CategoryCard from "@/components/CategoryCard";
 
 const Categories = (props) => {
   const { className } = props;
+  const categoryItems = [
+    {
+      title: "Action",
+      images: [
+        "/src/assets/images/categories/action/1.jpg",
+        "/src/assets/images/categories/action/2.jpg",
+        "/src/assets/images/categories/action/3.jpg",
+        "/src/assets/images/categories/action/4.jpg",
+      ],
+    },
+    {
+      title: "Adventure",
+      images: [
+        "/src/assets/images/categories/adventure/1.jpg",
+        "/src/assets/images/categories/adventure/2.jpg",
+        "/src/assets/images/categories/adventure/3.jpg",
+        "/src/assets/images/categories/adventure/4.jpg",
+      ],
+    },
+    {
+      title: "Comedy",
+      images: [
+        "/src/assets/images/categories/comedy/1.jpg",
+        "/src/assets/images/categories/comedy/2.jpg",
+        "/src/assets/images/categories/comedy/3.jpg",
+        "/src/assets/images/categories/comedy/4.jpg",
+      ],
+    },
+    {
+      title: "Drama",
+      images: [
+        "/src/assets/images/categories/drama/1.jpg",
+        "/src/assets/images/categories/drama/2.jpg",
+        "/src/assets/images/categories/drama/3.jpg",
+        "/src/assets/images/categories/drama/4.jpg",
+      ],
+    },
+    {
+      title: "Horror",
+      images: [
+        "/src/assets/images/categories/horror/1.jpg",
+        "/src/assets/images/categories/horror/2.jpg",
+        "/src/assets/images/categories/horror/3.jpg",
+        "/src/assets/images/categories/horror/4.jpg",
+      ],
+    },
+  ];
 
   return (
     <Section
@@ -19,7 +67,9 @@ const Categories = (props) => {
       }
       isActionsHiddenOnMobile
     >
-      Categories
+      {categoryItems.map((categoryItem, index) => (
+        <CategoryCard {...categoryItem} key={index} />
+      ))}
     </Section>
   );
 };
