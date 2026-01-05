d121635b432a4a05c2bf6ddebc154dd5dd7ef299
thisvolwebnik(Nikita) <thisvolwebnik@yandex.ru>
2026-01-05 17:26:28 +0300

    feat: add TabsCollection JavaScript module for tab management

---
+++ src/utils/pxToRem.js
@@ -0,0 +1,7 @@
+const pxToRem = (pixels) => {
+  const htmlElementFontSize = parseInt(getComputedStyle(document.documentElement).fontSize);
+
+  return pixels / htmlElementFontSize;
+};
+
+export default pxToRem;
